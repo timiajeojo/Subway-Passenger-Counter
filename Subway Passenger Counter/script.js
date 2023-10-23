18d6b800428e77fd6a1af26475d04ec8697dd3a5
Timi Ajeojo <timiajeojo@gmail.com>
2023-10-24 00:19:05 +0100

    added button

--- Subway Passenger Counter/script.js
+++ Subway Passenger Counter/script.js
@@ -1,1 +1,6 @@
-document.getElementById("count").innerText = 5
+//document.getElementById("count").innerText = 5
+
+let bonusPoints = 5
+bonusPoints = bonusPoints + 50
+bonusPoints = bonusPoints + 100
+console.log(bonusPoints);
